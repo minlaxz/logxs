da2737103aa85ba8d47d9f9a71e5e2376006b1eb
minlaxz <minminlaxz@gmail.com>
2020-09-21 18:35:37 +0630

    data type added.

--- logxs/logxs.py
+++ logxs/logxs.py
@@ -8,11 +8,25 @@ License: MIT, see LICENSE for more details.
 
 import logging
 
-def this(message):
-    logging.debug(message)
+class Logxs:
+    def __init__(self, ml=False, with_time=False, debug=True):
+        self.debug = debug
+        _format = "%(asctime)s: %(message)s" if with_time else "%(message)s"
+        _level = logging.DEBUG if self.debug else logging.INFO
+        logging.basicConfig(format=_format, level=_level, datefmt="%H:%M:%S")
+        self.ml = ml
+        logging.debug('use `out([any])` method.') if self.debug else logging.info('use `out([any])` method.')
+    
+    def out(self, *argv):
+        self.message = list()
+        for arg in argv:
+            if self.ml:
+                self.message.append(str(arg)+ ' {0}'.format(type(arg)))
+            else:
+                self.message.append(str(arg))
+        for m in self.message:
+            logging.debug(m) if self.debug else logging.info(m)
 
-format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.DEBUG, datefmt="%H:%M:%S")
-this('Logger Ready.')
+
 
 """ this? maybe I previously play with JS -'D """
