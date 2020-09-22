c033e25d150c3b154f3012f80877c13c8e8bdb33
minlaxz <minminlaxz@gmail.com>
2020-09-22 17:36:39 +0630

    version - 0.1.2 find_method added.

--- logxs/__init__.py
+++ logxs/__init__.py
@@ -11,4 +11,5 @@ from .__version__ import __title__, __description__, __url__, __version__
 from .__version__ import __author__, __author_email__, __license__
 
 from .logxs import *
+from .meta import find_method
 
